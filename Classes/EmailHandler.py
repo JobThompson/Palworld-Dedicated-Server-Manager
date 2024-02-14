e21e9b8bd73a56e62ae1035d7fba24497e5fd7b3
Job Thompson <pkwp38@gmail.com>
2024-02-13 23:29:39 -0500

    Updated email handler to allow emails to be sent out

--- Classes/EmailHandler.py
+++ Classes/EmailHandler.py
@@ -5,5 +5,35 @@ from email.mime.multipart import MIMEMultipart
 class EmailHandler:
     """Class that handles the email alerts for the server"""
     def __init__(self) -> None:
-        pass
+        self.smtp_server = None
+        self.smtp_port = None
+        self.smtp_user = None
+        self.smtp_password = None
     
+    
+    def create_email(self, to_email, subject = "Palworld Server Crash", body = "This email indicates that the Palworld server was not running. No worries though. The server was restarted. Beep beep boop."):
+        """Create an email with the given subject and body"""
+        msg = MIMEMultipart()
+        msg['From'] = self.smtp_user
+        msg['To'] = to_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        return msg.as_string()
+    
+    
+    def send_email(self, email_to, msg):
+        """Send the email to the given email address"""
+        try:
+            server = smtplib.SMTP(self.smtp_server, self.smtp_port)
+            server.starttls()
+
+            # Login to the email account
+            server.login(self.smtp_user, self.smtp_password)
+
+            # Send the email
+            server.sendmail(self.smtp_user, email_to, msg)
+
+            print("Sent notification email successfully.")
+
+        except smtplib.SMTPException:
+            pass
