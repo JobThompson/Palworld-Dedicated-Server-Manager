3f4cb3ff1ec5e2e11f8f34664d7f44cfe2fbc1af
Job Thompson <pkwp38@gmail.com>
2024-02-13 21:22:23 -0500

    Update MainRewrite.py

--- MainRewrite.py
+++ MainRewrite.py
@@ -1,6 +1,8 @@
 from Classes.Interface import Interface
+from Classes.EmailHandler import EmailHandler
 
 def main():
+    email_handler = EmailHandler()
     main_window = Interface()
 
 if __name__ == "__main__":
