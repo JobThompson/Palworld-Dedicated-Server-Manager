371a3249f65ac29eda9918964332a90728ea83c6
Job Thompson <pkwp38@gmail.com>
2024-02-15 21:50:02 -0500

    updated arrcon commands to use a match case to return the command requested.

--- Classes/Server.py
+++ Classes/Server.py
@@ -7,7 +7,7 @@ class Server:
         self.ip = ip
         self.port = port
         self.status = "offline"
-
+        
         self.rcon_pass = ''
         self.arrcon_exe_path = '../ARRCON/ARRCON.exe'
         self.rcon_port = ''
@@ -27,18 +27,30 @@ class Server:
         # Pull this from the local files? In order to use the RCON commands, a password and port has to be set in the server's config file.
         self.rcon_port = ''
         self.rcon_pass = ''
-
-    def define_server_commands(self):
-        """Define server commands based on form data"""
+        
+    def define_server_command_values(self):
+        """Defines the values needed for server commands based on form data"""
         self.rcon_port = form.rcon_port.cget("text")
         self.palworld_directory = form.server_directory_selection.cget("text")
         self.server_start_args = form.server_start_args_entry.get()
-        self.arrcon_command_save_server = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "save"'
-        self.arrcon_command_info_server = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "info"'
-        self.arrcon_command_shutdown_server = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "shutdown 60 The_server_will_be_restarting_in_60_seconds"'
-        self.arrcon_command_server_message_30 = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "broadcast The_server_will_be_restarting_in_30_seconds"'
-        self.arrcon_command_server_message_10 = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "broadcast The_server_will_be_restarting_in_10_seconds"'
-        self.start_server_command = f'{self.palworld_directory}/PalServer.exe {self.server_start_args}'
-        self.shutdown_server_command = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "shutdown 5 The_server_will_be_shutting_down_in_5_seconds"'
-        self.force_shutdown_server_command = f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "doexit"'
+
+    def get_arrcon_command(self, command):
+        """Returns the command to be executed based on the command passed in."""
+        match command:
+            case "save":
+                return f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "save"'
+            case "info":
+                return f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "info"'
+            case "shutdown":
+                return f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "shutdown 60 The_server_will_be_restarting_in_60_seconds"'
+            case "message_30":
+                return f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "broadcast The_server_will_be_restarting_in_30_seconds"'
+            case "message_10":
+                return  f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "broadcast The_server_will_be_restarting_in_10_seconds"'
+            case "start":
+                return f'{self.palworld_directory}/PalServer.exe {self.server_start_args}'
+            case "shutdown":
+                return f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "shutdown 5 The_server_will_be_shutting_down_in_5_seconds"'
+            case "force_shutdown":
+                return f'{self.arrcon_exe_path} -H 127.0.0.1 -P {self.rcon_port} -p {self.rcon_pass} "doexit"'
 
