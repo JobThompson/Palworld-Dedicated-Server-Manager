35028d709d4303961385602128b819bfbbad1292
Job Thompson <pkwp38@gmail.com>
2024-02-20 23:43:13 -0500

    removed old files

--- Rewrite/Classes/Interface/NotificationConfiguration.py
+++
@@ -1,7 +0,0 @@
-from customtkinter import CTkFrame
-
-class NotificationConfiguration(CTkFrame):
-    """Class to handle the notification configuration widgets."""
-    def __init__(self, master, **kwargs) -> None:
-        super().__init__(master, **kwargs)
-        self.label_font = ("Arial", 12, "bold")
